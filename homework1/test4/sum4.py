d8cf0bc07caa6adee4eea6509d209844cc656dd3
Daniil <danbychkov98@gmail.com>
2021-09-01 17:23:49 +0300

    done test4

--- homework1/test4/sum4.py
+++ homework1/test4/sum4.py
@@ -2,6 +2,12 @@ from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
+    """
+    Classic task, a kind of walnut for you
+    Given four lists A, B, C, D of integer values,
+        compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+    We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
+    """
     count = 0
     for i in a:
         for j in b:
