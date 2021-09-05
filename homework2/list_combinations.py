bfbee2bf9fa3f223b3f661b73091cb317d8edcae
Daniil <danbychkov98@gmail.com>
2021-09-05 19:33:48 +0300

    test3 done

--- homework2/list_combinations.py
+++ homework2/list_combinations.py
@@ -1,10 +1,27 @@
 from typing import List, Any
+from itertools import product
 
 
 def combinations(*args: List[Any]) -> List[List]:
-    for i in range(len(args)):
-        for j in range(len(args[i])):
-            print(args[i][j])
-
-print(combinations([1, 2], [3, 4], [5, 6]))
-
+    """
+    Write a function that takes K lists as arguments and returns all possible
+    lists of K items where the first element is from the first list,
+    the second is from the second and so one.
+    You may assume that that every list contain at least one element
+    Example:
+    assert combinations([1, 2], [3, 4]) == [
+        [1, 3],
+        [1, 4],
+        [2, 3],
+        [2, 4],
+    ]
+    """
+    flist = []
+    final_list = []
+    comb = list(product(*args))
+    for i in range(len(comb)):
+        for j in range(len(comb[i])):
+            flist.append(comb[i][j])
+        final_list.append(flist)
+        flist = []
+    return final_list
