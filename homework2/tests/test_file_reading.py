dfd8bbb78de245f3f6e16ae78ba7ec8b0100137b
Daniil <danbychkov98@gmail.com>
2021-09-08 16:48:45 +0300

    new paths

--- homework2/tests/test_file_reading.py
+++ homework2/tests/test_file_reading.py
@@ -6,9 +6,7 @@ from homework2.task1.file_reading import (count_non_ascii_chars,
 
 
 def test_longest_unique_symbols_words():
-    assert get_longest_diverse_words("C:\\Users\\Danya\\"
-                                     "Desktop\\EPAM Python 2021\\"
-                                     "homework2\\task1") \
+    assert get_longest_diverse_words("/homework2/tests/data.txt") \
            == ['unmißverständliche', 'Bevölkerungsabschub',
                'Kollektivschuldiger', 'Werkstättenlandschaft',
                'Schicksalsfiguren', 'Selbstverständlich',
@@ -17,19 +15,17 @@ def test_longest_unique_symbols_words():
 
 
 def test_rarest_char_in_text():
-    assert get_rarest_char("C:\\Users\\Danya\\Desktop\\"
-                           "EPAM Python 2021\\homework2\\"
-                           "task1\\data.txt")\
+    assert get_rarest_char("/homework2/tests/data.txt")\
             == "›, ‹, Y, î, ’, X, (, )"
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars("../task1/data.txt") == 5305
+    assert count_punctuation_chars("/homework2/tests/data.txt") == 5305
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars("../task1/data.txt") == 3352
+    assert count_non_ascii_chars("data.txt") == 3352
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char("../task1/data.txt") == ","
+    assert get_most_common_non_ascii_char("data.txt") == ","
