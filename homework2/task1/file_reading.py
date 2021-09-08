104b7d884b64a483b3ca15b8e6cc7e3b804e3e57
Daniil <danbychkov98@gmail.com>
2021-09-08 15:11:59 +0300

    fixes comments

--- homework2/task1/file_reading.py
+++ homework2/task1/file_reading.py
@@ -8,9 +8,8 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
     Find 10 longest words consisting from largest amount of unique symbols
     """
     with open(file_path, 'r', encoding="unicode-escape") as fi:
-        text = fi.read().splitlines()
+        text = fi.read()
         list_of_words_in_text = (''.join(ch for ch in text if ch not in string.punctuation)).split()
-    print(list_of_words_in_text)
     list_of_unique_symbols = []
     count_of_unique_symbols = 0
     dict_of_words_unique_sym = {}
@@ -70,10 +69,10 @@ def count_punctuation_chars(file_path: str) -> int:
     Count every punctuation char
     """
     count_of_punctuation_chars = 0
-    with open(file_path) as fi:
-        text = fi.read().splitlines()
+    with open(file_path, 'r', encoding="unicode-escape") as fi:
+        text = fi.read()
         for i in text:
-            if i in string.punctuatuin:
+            if i in string.punctuation:
                 count_of_punctuation_chars += 1
     return count_of_punctuation_chars
 
@@ -82,7 +81,7 @@ def count_non_ascii_chars(file_path: str) -> int:
     """
     Count every non ascii char
     """
-    with open(file_path) as fi:
+    with open(file_path, 'r', encoding="unicode-escape") as fi:
         text = fi.read().splitlines()
     count_of_non_ascii_chars = 0
     for i in text:
@@ -97,16 +96,14 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
     """
     count_of_every_non_ascii_char = 0
     dict_of_non_ascii_chars = defaultdict(int)
-    amount_of_non_ascii_chars = 0
-    with open(file_path) as fi:
-        text = fi.read().splitlines()
-        list_of_words_in_text = ''.join(ch for ch in text if ch not in string.ascii_letters)
+    with open(file_path, 'r', encoding="unicode-escape") as fi:
+        text = fi.read()
+        list_of_words_in_text = ''.join(ch for ch in text if ch not in string.ascii_letters and ch != ' ' and ch != '\n')
     for i in list_of_words_in_text:
         dict_of_non_ascii_chars[i] += 1
         count_of_every_non_ascii_char += 1
-    for k, v in dict_of_non_ascii_chars.items():
-        if v > count_of_every_non_ascii_char / 2 and v > amount_of_non_ascii_chars:
-            amount_of_non_ascii_chars = v
-    for k, v in dict_of_non_ascii_chars.items():
-        if v == amount_of_non_ascii_chars:
-            return k
+
+    def value_getter(a):
+        return dict_of_non_ascii_chars.get(a)
+
+    return max(dict_of_non_ascii_chars, key=value_getter)
--- homework2/tests/test_file_reading.py
+++ homework2/tests/test_file_reading.py
@@ -5,29 +5,29 @@ from homework2.task1.file_reading import get_longest_diverse_words,\
 
 
 def test_longest_unique_symbols_words():
-    assert get_longest_diverse_words("../task1/data.txt") == ['kalyptischen',
-                                                     'Augenblick,',
-                                                     'Grundmotiv,',
-                                                     'Evangelium,',
-                                                     'Deutschland',
-                                                     'unsterblich',
-                                                     'vorfindet.',
-                                                     'Schauspiel',
-                                                     'Bolschewik',
-                                                     'Sicherung.']
+    assert get_longest_diverse_words("data.txt") == ['unmißverständliche',
+                                                              'Bevölkerungsabschub',
+                                                              'Kollektivschuldiger',
+                                                              'Werkstättenlandschaft',
+                                                              'Schicksalsfiguren',
+                                                              'Selbstverständlich',
+                                                              'Fingerabdrucks',
+                                                              'Friedensabstimmung',
+                                                              'außenpolitisch',
+                                                              'Seinsverdichtungen']
 
 
 def test_rarest_char_in_text():
-    assert get_rarest_char("../task1/data.txt") == "Y, X, (, )"
+    assert get_rarest_char("data.txt") == "›, ‹, Y, î, ’, X, (, )"
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars("../task1/data.txt") == 8277
+    assert count_punctuation_chars("data.txt") == 5305
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars("../task1/data.txt") == 15880
+    assert count_non_ascii_chars("data.txt") == 3352
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char("../task1/data.txt") == "0"
+    assert get_most_common_non_ascii_char("data.txt") == ","
