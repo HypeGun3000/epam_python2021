0f6b69a8ee67bc42a96e8d5ae57d6d42aecce83b
Daniil <danbychkov98@gmail.com>
2021-09-08 23:15:18 +0300

    first task done

--- homework3/task1/cache_parametrized.py
+++ homework3/task1/cache_parametrized.py
@@ -1,22 +1,32 @@
+import pickle
 from typing import Callable
 
 
-def cache(func: Callable) -> Callable:
-    cache_dict = {}
+def cache(times):
+    count = times
 
-    def wrapper(*args, **kwargs):
-        nonlocal cache_dict
-        key = (args, tuple(kwargs.items()))
-        if key not in cache_dict:
-            response = func(*args, **kwargs)
-            cache_dict[key] = response
-        return cache_dict[key]
-    return wrapper
+    def func(function: Callable) -> Callable:
+        cache_dictionary = {}
+
+        def wrapper(*args, **kwargs):
+            nonlocal times
+            nonlocal count
+            nonlocal cache_dictionary
+            if count == 0:
+                cache_dictionary = {}
+                count = times
+            key = (args, tuple(kwargs.items()))
+            bite_code = pickle.dumps(key)
+            if bite_code not in cache_dictionary:
+                response = function(*args, **kwargs)
+                cache_dictionary[bite_code] = response
+            print(cache_dictionary[bite_code])
+            count -= 1
+            return cache_dictionary
+        return wrapper
+    return func
 
 
-@cache(time=2)
-def f(a, b):
-    return a*b
-
-f(10, 2)
-print(f(10, 2))
+@cache(times=2)
+def f():
+    return input("? ")
---
+++ homework3/tests/test_task1.py
@@ -0,0 +1,5 @@
+from homework3.task1.cache_parametrized import f
+
+
+def test_three_times():
+    pass
