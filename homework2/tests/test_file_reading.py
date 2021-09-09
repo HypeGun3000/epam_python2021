bfd8afc8290dac6bb9adc1b33af723d8c4c91d7a
Daniil <danbychkov98@gmail.com>
2021-09-09 17:44:58 +0300

    add path

--- homework2/tests/test_file_reading.py
+++ homework2/tests/test_file_reading.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 from homework2.task1.file_reading import (count_non_ascii_chars,
                                           count_punctuation_chars,
@@ -6,9 +6,12 @@ from homework2.task1.file_reading import (count_non_ascii_chars,
                                           get_most_common_non_ascii_char,
                                           get_rarest_char)
 
+root = Path(__file__).parent
+data_file = root / 'data.txt'
+
 
 def test_longest_unique_symbols_words():
-    assert get_longest_diverse_words(os.path.abspath("data.txt")) \
+    assert get_longest_diverse_words(data_file) \
            == ['unmißverständliche', 'Bevölkerungsabschub',
                'Kollektivschuldiger', 'Werkstättenlandschaft',
                'Schicksalsfiguren', 'Selbstverständlich',
@@ -17,17 +20,17 @@ def test_longest_unique_symbols_words():
 
 
 def test_rarest_char_in_text():
-    assert get_rarest_char(os.path.abspath("data.txt")) == "›"
+    assert get_rarest_char(data_file) == "›"
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars(os.path.abspath("data.txt")) == 5305
+    assert count_punctuation_chars(data_file) == 5305
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars(os.path.abspath("data.txt")) == 3352
+    assert count_non_ascii_chars(data_file) == 3352
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char(os.path.abspath("data.txt"))\
+    assert get_most_common_non_ascii_char(data_file)\
            == ","
