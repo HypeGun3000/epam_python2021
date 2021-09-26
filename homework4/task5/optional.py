76c4067e8f09d49b3ee5219a70484bd4be091bb6
Daniil <danbychkov98@gmail.com>
2021-09-27 00:20:40 +0300

    done task 5

--- homework4/task5/optional.py
+++ homework4/task5/optional.py
@@ -7,7 +7,7 @@ Definition of done:
  - function is created
  - function is properly formatted
  - function has tests
-#>>> list(fizzbuzz(5))
+>>> list(fizzbuzz(5))
 ["1", "2", "fizz", "4", "buzz"]
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** https://www.youtube.com/watch?v=NSzsYWckGd4
@@ -15,10 +15,19 @@ Definition of done:
 from typing import List, Generator
 
 
-def fizzbuzz(n: int) -> Generator[str]:
-    fizz = ""*2 + "fizz"
-    buzz = ""*5 + "buzz"
-    fizzbuzz = ""*15 + "fizzbuzz"
-    yield fizz[n] + buzz[n] + fizzbuzz[n]
+def fizzbuzz(n: int) -> Generator[str, None, None]:
+    gen_fizzbuzz = []
+    for i in range(1, n+1):
+        gen_fizzbuzz.append(str(i))
+    for i in range(2, n, 3):
+        gen_fizzbuzz[i] = "fizz"
+    for i in range(4, n, 5):
+        gen_fizzbuzz[i] = "buzz"
+    for i in range(14, n, 15):
+        gen_fizzbuzz[i] = "fizzbuzz"
+    count = 0
+    while count != n:
+        count += 1
+        yield gen_fizzbuzz[count - 1]
 
-fizzbuzz(5)
+print(list(fizzbuzz(25)))
