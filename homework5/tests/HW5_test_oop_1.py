3e5571be23a5e37199fac51cca1689ffb05bcbd6
Daniil <danbychkov98@gmail.com>
2021-09-27 18:22:46 +0300

    fix isort

--- homework5/tests/HW5_test_oop_1.py
+++ homework5/tests/HW5_test_oop_1.py
@@ -1,7 +1,7 @@
 from unittest.mock import Mock
 
 import pytest
-from homework5.task1.oop_1 import Homework, Student, Teacher
+from homework5.task1.oop_1 import (Homework, Student, Teacher)
 
 
 class TestOOP1:
