ccb511c38bd04614a8acd8bd3783703cf88f5626
Daniil <danbychkov98@gmail.com>
2021-09-27 18:13:19 +0300

    fix comments

--- homework5/task1/oop_1.py
+++ homework5/task1/oop_1.py
@@ -2,31 +2,34 @@ from datetime import timedelta, datetime
 
 
 class Homework:
-    def __init__(self, text, deadline):
+    def __init__(self, text: str, deadline: int):
         self.text = text
-        self.deadline = timedelta(days=deadline)
+        if deadline >= 0:
+            self.deadline = timedelta(days=deadline)
+        else:
+            raise ValueError("Deadline must be more then 0, or equal 0")
         self.created = datetime.now()
 
     def is_active(self):
-        return self.created + self.deadline > datetime.now()
+        return self.created + self.deadline < datetime.now()
 
 
 class Student:
-    def __init__(self, last_name, first_name):
+    def __init__(self, last_name: str, first_name: str):
         self.last_name = last_name
         self.first_name = first_name
 
     def do_homework(self, homework: Homework):
-        if homework.is_active() is True:
+        if homework.is_active():
             return homework
         else:
             print("You are late")
 
 
 class Teacher:
-    def __init__(self, last_name, first_name):
+    def __init__(self, last_name: str, first_name: str):
         self.last_name = last_name
         self.first_name = first_name
 
-    def create_homework(self, text, deadline):
+    def create_homework(self, text: str, deadline: int):
         return Homework(text, deadline)
