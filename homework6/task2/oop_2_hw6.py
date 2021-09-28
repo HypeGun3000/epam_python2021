ce6031cba3ab3f29917d0ed1354f7129c1f21982
Daniil <danbychkov98@gmail.com>
2021-09-28 14:58:03 +0300

    oop2 fix

--- homework6/task2/oop_2_hw6.py
+++ homework6/task2/oop_2_hw6.py
@@ -1,39 +1,47 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 
 
 class Human:
-    def __init__(self, last_name, first_name):
+    def __init__(self, last_name: str, first_name: str):
         self.last_name = last_name
         self.first_name = first_name
 
 
-class Homework:
-    def __init__(self, text, deadline):
-        self.text = text
-        self.deadline = timedelta(days=deadline)
-        self.created = datetime.now()
-
-    def is_active(self):
-        return self.created + self.deadline > datetime.now()
-
-
-class Student(Human):
-    def do_homework(self, homework: Homework):
-        if homework.is_active() is True:
-            return homework
-        raise DeadlineError("You are late")
-
-
-class Teacher(Human):
-    def create_homework(self, text, deadline):
-        return Homework(text, deadline)
+class DeadlineError(Exception):
+    def __init__(self, message="You are late"):
+        self.message = message
 
 
 class HomeworkResult:
-    def __init__(self, homework, solution, author, created):
+    def __init__(self, homework, solution: str):
         if not isinstance(homework, Homework):
-            print("You gave a not Homework object")
+            raise TypeError("You gave a not Homework object")
         self.solution = solution
         self.author = Student
         self.created = datetime.now()
 
+
+class Homework:
+    def __init__(self, text: str, deadline: int):
+        self.text = text
+        if deadline >= 0:
+            self.deadline = timedelta(days=deadline)
+        else:
+            raise ValueError("Deadline must be more then 0, or equal 0")
+        self.created = datetime.now()
+
+    def is_active(self):
+        return self.created + self.deadline < datetime.now()
+
+
+class Student(Human):
+    def do_homework(self, homework: Homework):
+        if homework.is_active():
+            return homework
+        else:
+            raise DeadlineError
+
+
+class Teacher(Human):
+    def create_homework(self, text: str, deadline: int):
+        return Homework(text, deadline)
