88a21f1f097fc538fc4e300d4ad8fc4491f160c2
Daniil <danbychkov98@gmail.com>
2021-09-28 16:16:20 +0300

    fix lines

--- homework4/tests/HW4_test_task1.py
+++ homework4/tests/HW4_test_task1.py
@@ -7,7 +7,7 @@ from homework4.task1.read_file import check_file, read_magic_number
 
 class TestMagicNumber:
     @pytest.fixture
-    def create_file_with_wrong_first_line(self, tmp_path):
+    def create_file_wrong_first_line(self, tmp_path):
         test_file_path = os.path.join(tmp_path, "data.txt")
         test_data = "moommy\n51\n2"
         with open(test_file_path, "w") as file_open:
@@ -15,20 +15,18 @@ class TestMagicNumber:
         return test_file_path
 
     @pytest.fixture
-    def create_file_with_true_first_line(self, tmp_path):
+    def create_file_true_first_line(self, tmp_path):
         test_file_path = os.path.join(tmp_path, "data2.txt")
         test_data = "2\n5415wjdglsg"
         with open(test_file_path, "w") as file_open:
             file_open.writelines(test_data)
         return test_file_path
 
-    def test_if_file_with_wrong_first_line_created(self,
-                                            create_file_with_wrong_first_line):
-        assert check_file(create_file_with_wrong_first_line) is True
+    def test_if_file_wrong_first_line_created(self, create_file_wrong_first_line):
+        assert check_file(create_file_wrong_first_line) is True
 
-    def test_if_file_with_true_first_line_created(self,
-                                            create_file_with_true_first_line):
-        assert check_file(create_file_with_true_first_line) is True
+    def test_if_file_true_first_line_created(self, create_file_true_first_line):
+        assert check_file(create_file_true_first_line) is True
 
     def test_with_normal_first_line(self, create_file_with_true_first_line):
         assert read_magic_number(create_file_with_true_first_line) is True
