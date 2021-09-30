24f0ef4025bac2347311e152e25163f3ab4c01ad
Daniil <danbychkov98@gmail.com>
2021-09-30 10:42:54 +0300

    add tests

--- homework4/tests/HW4_test_task2.py
+++ homework4/tests/HW4_test_task2.py
@@ -1,6 +1,8 @@
 import urllib
 from unittest.mock import Mock
 
+import pytest
+
 from homework4.task2.mock_input import count_dots_on_i
 
 
@@ -11,7 +13,5 @@ class TestCountDotsOnUrl:
         assert count_dots_on_i("xxxx") == 3
 
     def test_not_exist_url(self):
-        try:
-            count_dots_on_i("http://wewwwewe.com/")
-        except urllib.error.URLError:
-            print("test passed")
+        with pytest.raises(urllib.error.URLError):
+            assert count_dots_on_i("https://asfas3332221fas.com") is True
