1b020bb7287b3a69fa427f48c62e3b64ea69777f
Daniil <danbychkov98@gmail.com>
2021-10-06 04:33:22 +0300

    trying task2

--- homework6/task2/oop_2_hw6.py
+++ homework6/task2/oop_2_hw6.py
@@ -37,24 +37,68 @@ class Homework:
 
 
 class Student(Human):
-    def do_homework(self, homework: Homework):
+    def do_homework(self, homework: Homework, solution):
         if homework.is_active():
-            return homework
-        else:
             raise DeadlineError("You are late")
+        else:
+            return HomeworkResult(homework, solution)
 
 
 class Teacher(Human):
-    homework_done = defaultdict(HomeworkResult)
+    homework_done = defaultdict(list)
+    _homework_db = defaultdict(list)
+
+    def check_duplicate(self, homework_result):
+        hw_info = (
+            homework_result.homework,
+            homework_result.solution
+        )
+        if self._homework_db[homework_result.homework]:
+            for homework in self._homework_db[homework_result]:
+                print(homework)
+                if homework[0] == hw_info[0] and homework[1] == hw_info[1]:
+                    return False
+        self._homework_db[homework_result.homework].append(hw_info)
+        return True
+
 
     def create_homework(self, text: str, deadline: int):
         return Homework(text, deadline)
 
     def check_homework(self, homework_result):
-        if len(homework_result.solutuion) < 5:
-            return False
-        else:
-            if HomeworkResult not in homework_done[Homework]:
-                homework_done[Homework] = HomeworkResult
+        if len(homework_result.solution) > 5 and self.check_duplicate(homework_result):
+            self.homework_done[homework_result.homework].append(homework_result)
+            return True
+        return False
 
 
+
+if __name__ == '__main__':
+    opp_teacher = Teacher('Daniil', 'Shadrin')
+    advanced_python_teacher = Teacher('Aleksandr', 'Smetanin')
+
+    lazy_student = Student('Roman', 'Petrov')
+    good_student = Student('Lev', 'Sokolov')
+
+    oop_hw = opp_teacher.create_homework('Learn OOP', 1)
+    docs_hw = opp_teacher.create_homework('Read docs', 5)
+
+    result_1 = good_student.do_homework(oop_hw, 'I have done this hw')
+    result_2 = good_student.do_homework(docs_hw, 'I have done this hw too')
+    result_3 = lazy_student.do_homework(docs_hw, 'I have done this hw too')
+    try:
+        result_4 = HomeworkResult(good_student, "fff", "Solution")
+    except Exception:
+        print('There was an exception here')
+    print(opp_teacher.check_homework(result_1))
+    temp_1 = opp_teacher.homework_done
+
+    advanced_python_teacher.check_homework(result_1)
+    temp_2 = Teacher.homework_done
+    assert temp_1 == temp_2
+
+    print(opp_teacher.check_homework(result_2))
+    print(opp_teacher.check_homework(result_3))
+
+    print(Teacher.homework_done[oop_hw])
+    #Teacher.reset_results()
