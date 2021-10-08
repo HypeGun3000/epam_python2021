0cba3825f4b88bd308c7a8313f7e0454bd003332
Daniil <danbychkov98@gmail.com>
2021-10-08 14:52:18 +0300

    add hw 7

---
+++ homework7/task1/find_in_tree.py
@@ -0,0 +1,39 @@
+import itertools
+from typing import Any
+
+# Example tree:
+example_tree = {
+    "first": ["RED", "BLUE"],
+    "second": {
+        "simple_key": ["simple", "list", "of", "RED", "valued"],
+    },
+    "third": {
+        "abc": "BLUE",
+        "jhl": "RED",
+        "complex_key": {
+            "key1": "value1",
+            "key2": "RED",
+            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
+            "key4": ["RED", "RED", {"qrdfasf": "RED", "ASF": "RED"}]
+        }
+     },
+    "fourth": "RED"
+}
+
+list_of_element = []
+
+
+def find_occurrences(tree: dict, element: Any) -> int:
+    for value in itertools.chain(tree.values()):
+        if isinstance(value, dict):
+            find_occurrences(value, element)
+        elif isinstance(value, list):
+            for i in value:
+                if isinstance(i, dict):
+                    find_occurrences(i, element)
+                if i == element:
+                    list_of_element.append(i)
+        elif isinstance(value, str):
+            if value == element:
+                list_of_element.append(value)
+    return len(list_of_element)
---
+++ homework7/tests/HW7_task3.py
@@ -0,0 +1,33 @@
+import pytest
+
+from homework7.task3.tic_tac_toe import tic_tac_toe_checker
+
+
+class TestTicTacToe:
+    @pytest.fixture
+    def vertical_victory_o(self):
+        return [["o", "-", "o"], ["o", "x", "o"], ["o", "x", "x"]]
+
+    @pytest.fixture
+    def horizontal_victory_x(self):
+        return [["-", "-", "o"], ["-", "o", "o"], ["x", "x", "x"]]
+
+    @pytest.fixture
+    def diagonal_victory_x(self):
+        return [["x", "-", "o"], ["-", "x", "o"], ["x", "o", "x"]]
+
+    @pytest.fixture
+    def unfinished(self):
+        return [["x", "-", "o"], ["-", "x", "o"], ["x", "o", "-"]]
+
+    def test_vertical_victory(self, vertical_victory_o):
+        assert tic_tac_toe_checker(vertical_victory_o) == 'o wins!'
+
+    def test_horizontal_victory(self, horizontal_victory_x):
+        assert tic_tac_toe_checker(horizontal_victory_x) == 'x wins!'
+
+    def test_diagonal_victory(self, diagonal_victory_x):
+        assert tic_tac_toe_checker(diagonal_victory_x) == 'x wins!'
+
+    def test_unfinished(self, unfinished):
+        assert tic_tac_toe_checker(unfinished) == 'unfinished'
