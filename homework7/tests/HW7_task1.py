53fa0fc07ad0f635389a9f71e5a31eace272461a
Daniil <danbychkov98@gmail.com>
2021-10-08 15:00:41 +0300

    delete hw7

--- homework7/tests/HW7_task1.py
+++
@@ -1,9 +0,0 @@
-from homework7.task1.find_in_tree import example_tree, find_occurrences
-
-
-class TestTree:
-    def test_example_tree(self):
-        assert find_occurrences(example_tree, "RED") == 10
-
-    def test_example_tree_with_wrong_color(self):
-        assert find_occurrences(example_tree, "PURPLE") == 0
