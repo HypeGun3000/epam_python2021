c7387aa08aafd6cdacfbf03330cea6ce9ee5d3f1
Daniil <danbychkov98@gmail.com>
2021-10-18 19:07:09 +0300

    done hw9

--- homework9/task3/count_tokenaizer.py
+++ homework9/task3/count_tokenaizer.py
@@ -3,47 +3,36 @@ from typing import Optional, Callable
 
 import os
 
-
 dir_path = os.getcwd()
 
 
 def get_files_with_extension(dir_path: Path, file_extension: str):
     files_with_extension = []
-    try:
-        for file in os.listdir(dir_path):
-            if file.endswith(file_extension):
-                files_with_extension.append(file)
-        return files_with_extension
-    except NotADirectoryError:
-        return "Wrong path"
-
-def using_tokenizer(dir_path: Path, file_extension: str):
-    print(get_files_with_extension(dir_path, file_extension))
-
-print(using_tokenizer(dir_path, ".txt"))
-
-
-
-
-
-def universal_file_counter(
-    dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
-) -> int:
-    """
-    Write a function that takes directory path, a file extension and an optional tokenizer.
-    It will count lines in all files with that extension if there are no tokenizer.
-    If a the tokenizer is not none, it will count tokens.
-    For dir with two files from hw1.py:
-    >>> universal_file_counter(test_dir, "txt")
-    6
-    >>> universal_file_counter(test_dir, "txt", str.split)
-    6
-    """
-    count = 0
-    files_with_extension = []
+    if not os.path.isdir(dir_path):
+        raise NotADirectoryError("It's not a directory")
     for file in os.listdir(dir_path):
-        if file.endswith(".txt"):
+        if file.endswith(file_extension):
             files_with_extension.append(file)
-    print(files_with_extension)
+    return files_with_extension
 
-print(universal_file_counter(dir_path, "txt", str.split))
+
+def universal_file_counter(dir_path: Path, file_extension: str,
+                           tokenizer: Optional[Callable] = None):
+    count_of_lines = 0
+    count_of_tokens = 0
+    if tokenizer is None:
+        for i in get_files_with_extension(dir_path, file_extension):
+            with open(os.path.join(dir_path, i)) as file:
+                count_of_lines += len(file.readlines())
+        return count_of_lines
+
+    for i in get_files_with_extension(dir_path, file_extension):
+        with open(os.path.join(dir_path, i)) as file:
+            for line in file.readlines():
+                if tokenizer(line):
+                    count_of_tokens += 1
+        return count_of_tokens
+
+
+print(universal_file_counter(dir_path, ".txt"))
+print(universal_file_counter(dir_path, ".txt", str.split))
---
+++ homework9/tests/HW9_test_task3.py
@@ -0,0 +1,21 @@
+from homework9.task3.count_tokenaizer import universal_file_counter
+import pytest
+import os
+
+from pathlib import Path
+
+
+class TestUniFileCounter:
+    test_dir = Path(__file__).parents[0]
+
+    def test_files_without_tokens(self):
+        assert universal_file_counter(self.test_dir, "txt") == 11
+
+    def test_files_with_token(self):
+        assert universal_file_counter(self.test_dir, "txt", str.split) == 9
+
+    def test_not_real_directory(self):
+        with pytest.raises(NotADirectoryError) as error:
+            assert universal_file_counter("asdasd/afasf/iew", "txt")
+        err_msg = error.value.args[0]
+        assert err_msg == "It's not a directory"
