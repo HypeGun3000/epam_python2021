649024389606b54c0b0a36e468a757c46762dee4
Daniil <danbychkov98@gmail.com>
2021-10-19 03:54:18 +0300

    add hw11

---
+++ homework11/tests/HW11_test_task1.py
@@ -0,0 +1,19 @@
+from homework11.task1.remove_duplicate_metaclass import SimplifiedEnum
+
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
+
+
+class TestMetaClass:
+    def test_attribute_from_ColorsEnum(self):
+        assert ColorsEnum.RED == "RED"
+
+    def test_attribute_from_SizesEnum(self):
+        assert SizesEnum.XL == "XL"
+
+
