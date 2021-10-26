6bcf721f4129e459a52e4c20d267a12c4a5034e6
Daniil <danbychkov98@gmail.com>
2021-10-26 13:27:48 +0300

    last fixes flake

--- homework10/parc_titles_of_companies.py
+++ homework10/parc_titles_of_companies.py
@@ -10,12 +10,11 @@ from homework10.parc_cb_cours import get_valute_course_in_rubles
 url_site = "https://markets.businessinsider.com/index/components/s&p_500?p="
 
 headers = {
-        "accept": "*/*",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-                      " AppleWebKit/537.36 (KHTML, like Gecko)"
-                      " Chrome/94.0.4606.81 Safari/537.36"
-    }
-
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                  " AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/94.0.4606.81 Safari/537.36"
+}
 
 list_of_information_from_all_title_pages = []
 list_of_all_links = []
@@ -31,7 +30,7 @@ def get_title_information_from_all_pages(url_site: str, headers: dict):
         src = req_from_site.text
         soup = BeautifulSoup(src, "lxml")
         try:
-            all_companies_info_title =\
+            all_companies_info_title = \
                 soup.find(class_="table__tbody").find_all("tr")
         except AttributeError:
             return list_of_information_from_all_title_pages
@@ -40,9 +39,9 @@ def get_title_information_from_all_pages(url_site: str, headers: dict):
             information_by_lines = i.text.strip().splitlines()
             for line in information_by_lines:
                 if line != '' and "\t" not in line:
-                    list_of_information_from_one_page.\
+                    list_of_information_from_one_page. \
                         append(line.replace(',', ''))
-            list_of_information_from_all_title_pages.\
+            list_of_information_from_all_title_pages. \
                 append(list_of_information_from_one_page)
         count_page += 1
 
@@ -57,7 +56,7 @@ def get_links_for_all_companies(headers: dict):
         src = request.text
         soup = BeautifulSoup(src, 'lxml')
         try:
-            links =\
+            links = \
                 soup.find(class_="table__tbody").find_all("a")
         except AttributeError:
             return list_of_all_links
@@ -82,22 +81,22 @@ def get_info_from_company(link: str):
     company_code = soup.find(class_="price-section__category")
     company_name = soup.find(class_="price-section__label")
     company_p_e = soup.find_all(class_="snapshot__data-item")
-    company_potential_profit_low =\
+    company_potential_profit_low = \
         soup.find_all(class_="snapshot__data-item snapshot__data-item--small")
     company_potential_profit_high = soup.find_all(
         class_="snapshot__data-item snapshot__data-item--small"
                " snapshot__data-item--right")
 
     list_of_one_company_code_name.append(company_name.text.strip())
-    list_of_one_company_code_name.\
+    list_of_one_company_code_name. \
         append(company_code.text.strip().split(', ')[-1])
     list_of_one_company_code_name.append(False)
     for i in company_p_e:
         if "P/E Ratio" in i.text:
             ratio_company = i.text.strip().split()
-            list_of_one_company_code_name[-1] =\
+            list_of_one_company_code_name[-1] = \
                 ratio_company[0].replace(',', '')
-            list_of_one_company_code_name[-1] =\
+            list_of_one_company_code_name[-1] = \
                 float(list_of_one_company_code_name[-1])
             break
     list_of_one_company_code_name.append(False)
@@ -108,7 +107,7 @@ def get_info_from_company(link: str):
     for i in company_potential_profit_high:
         if "Week" in i.text:
             high_week = float(i.text.strip().split()[0].replace(",", ""))
-            list_of_one_company_code_name[-1] =\
+            list_of_one_company_code_name[-1] = \
                 round((high_week - list_of_one_company_code_name[-1]), 2)
     company_page_info.append(list_of_one_company_code_name)
     return list_of_one_company_code_name
@@ -117,7 +116,7 @@ def get_info_from_company(link: str):
 def get_price_in_rubles():
     global company_page_info
     for i in range(len(company_page_info)):
-        company_page_info[i].\
+        company_page_info[i]. \
             append(list_of_information_from_all_title_pages[i][1])
     return company_page_info
 
@@ -125,7 +124,7 @@ def get_price_in_rubles():
 def get_year_groth():
     global company_page_info
     for i in range(len(company_page_info)):
-        company_page_info[i].\
+        company_page_info[i]. \
             append(list_of_information_from_all_title_pages[i][-1])
     return company_page_info
 
@@ -139,8 +138,9 @@ def convert_dollars_in_rub(company: list):
 def multiprocessing_convert():
     global company_page_info
     with Pool(50) as pool:
-        company_page_info = pool.map(convert_dollars_in_rub,
-                                     [company for company in company_page_info])
+        company_page_info = \
+            pool.map(convert_dollars_in_rub,
+                     [company for company in company_page_info])
     return company_page_info
 
 
@@ -176,7 +176,6 @@ def add_json_files(file_name: str, top_companies: list):
 
 
 if __name__ == "__main__":
-
     start_time = datetime.datetime.now()
     get_links_for_all_companies(headers)
     get_information_from_every_company_page(get_info_from_company)
@@ -185,16 +184,16 @@ if __name__ == "__main__":
     get_year_groth()
     multiprocessing_convert()
 
-    top_10_price =\
+    top_10_price = \
         sorted(company_page_info,
                key=lambda rubl: rubl[4], reverse=True)[:10]
-    top_10_lowest_p_e =\
+    top_10_lowest_p_e = \
         sorted(company_page_info,
                key=lambda pe: pe[2], reverse=False)[:10]
-    top_10_groth =\
+    top_10_groth = \
         sorted(company_page_info,
                key=lambda groth: float((groth[5])[:-1]), reverse=True)[:10]
-    top_10_potential_profit =\
+    top_10_potential_profit = \
         sorted(company_page_info,
                key=lambda pp: pp[3], reverse=True)[:10]
     add_json_files("top_10_price.json",
@@ -206,4 +205,3 @@ if __name__ == "__main__":
     add_json_files("top_10_potential_profit.json",
                    add_information_in_dicts(top_10_potential_profit))
     print(datetime.datetime.now() - start_time)
-
