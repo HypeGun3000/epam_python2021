9fe421ffea1cb9b2862636ce14f0d715a5dcb3ce
Daniil <danbychkov98@gmail.com>
2021-10-27 19:20:47 +0300

    delete secret key

--- homework12/homework12/settings.py
+++ homework12/homework12/settings.py
@@ -19,10 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
-    'django-insecure-z1fa8!b0as@y(fgkp9wb9_lhavognq@w&1(&@oo#7dd%*g_it%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
